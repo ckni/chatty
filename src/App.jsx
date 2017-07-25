d7036ba2679f757356a6f622f373e4e181b73b38
Chester Ni <kaichesterni@gmail.com>
2017-07-25 21:29:17 +0000

    Abstracted sample messages into an in memory object

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,33 @@ import MessageList from "./components/MessageList.jsx";
 import Chatbar from "./components/Chatbar.jsx";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: {
+        name: "Bob" // optional. if currentUser is not defined, it means the user is Anonymous
+      },
+      messages: [
+        {
+          id: 0,
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          id: 1,
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
         <Navbar />
-        <MessageList />
-        <Chatbar />
+        <MessageList messages={ this.state.messages }/>
+        <Chatbar username={ this.state.currentUser.name }/>
       </div>
     );
   }
--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -4,9 +4,12 @@ import Message from './Message.jsx';
 
 class MessageList extends Component {
   render() {
+    const listMessages = this.props.messages.map(message =>
+      <Message key={ message.id } username={ message.username } content={ message.content } /> // TODO: Assign an ID to each message
+    );
     return (
       <main className="messages">
-        <Message />
+        { listMessages }
       </main>
     );
   }
