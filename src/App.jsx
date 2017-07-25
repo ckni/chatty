d432d769fc22e03a5991ff72c292d2566d00f73e
Chester Ni <kaichesterni@gmail.com>
2017-07-25 20:35:05 +0000

    Modularized static webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+
+import Navbar from "./components/Navbar.jsx";
+import MessageList from "./components/MessageList.jsx";
+import Chatbar from "./components/Chatbar.jsx";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar">
-          <a href="/" className="navbar-brand">Chatty</a>
-        </nav>
-        <main className="messages">
-          <div className="message">
-            <span className="message-username">Anonymous1</span>
-            <span className="message-content">I won't be impressed with technology until I can download food.</span>
-          </div>
-          <div className="message system">
-            Anonymous1 changed their name to nomnom.
-          </div>
-        </main>
-        <footer className="chatbar">
-          <input className="chatbar-username" placeholder="Your Name (Optional)" />
-          <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-        </footer>
+        <Navbar />
+        <MessageList />
+        <Chatbar />
       </div>
     );
   }
